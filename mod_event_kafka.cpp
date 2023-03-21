0f6e958a6ced470756b5649fb2f9c93f4db2654b
Justin-lavelle <justinl@broadvoice.com>
2023-03-20 20:10:21 -0700

    revert load config reload

--- mod_event_kafka.cpp
+++ mod_event_kafka.cpp
@@ -86,7 +86,7 @@ namespace mod_event_kafka {
 
             switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "KafkaEventPublisher Initialising...");
 
-            load_config(SWITCH_TRUE);
+            load_config(SWITCH_FALSE);
             
             conf = rd_kafka_conf_new();
 
