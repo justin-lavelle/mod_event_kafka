9942da420b71aafb83cf0ed98b2d0b378367d9e8
Justin-lavelle <justinl@broadvoice.com>
2023-03-23 23:57:16 -0700

    try to fix cat VERSION

--- distribution/make-deb.sh
+++ distribution/make-deb.sh
@@ -4,7 +4,7 @@ set -ex
 BUILD_ROOT=$(mktemp -d)
 NAME=mod-event-kafka
 SUB_VERSION=$(git rev-list --count HEAD)
-VERSION=$(shell cat VERSION)-${SUB_VERSION}
+VERSION=$(cat VERSION)-${SUB_VERSION}
 ARCH=$(dpkg --print-architecture)
 
 make
